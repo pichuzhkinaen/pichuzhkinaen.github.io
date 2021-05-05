f8e3f902e1df899715b69ea0d44ccdd1e0c364e8
pichuzhkinaen <88-katerina@mail.ru>
2021-05-05 16:20:59 +0300

    add section contacts, anchor-links, fix section

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    // const portfolioItem = document.querySelectorAll('.portfolio__item'),
+    //       portfilioBtn = document.querySelector('.portfolio__button');
+
+    // openPortfolioItem();
+    // portfilioBtn.addEventListener('click', openPortfolioItem);
+    
+    
+    // for (let i = 0; i < portfolioItem.length; i++) {
+    //     if (i < 6) {
+    //         portfolioItem[i].classList.add('active');
+    //     }
+    // }
+    function openPortfolioItem() {
+        
+        // const portfolioItemActive = document.querySelectorAll('.portfolio__item .active');
+        // for (let i = 0; i < portfolioItem.length; i++) {
+        //     if (portfolioItem[i].classList.contain('active')) {
+        //         portfolioItem[i].classList.add('active');
+        //     }
+        // }
+    }
+});
